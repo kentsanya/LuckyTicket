8af5b71541288b1a1bccd9abc39a4d4f71bc3000
Oleksandr Vdovitchenko <kentsanya91@gmail.com>
2021-02-19 12:03:29 +0200

    fix indices

--- LuckyTicket/Program.cs
+++ LuckyTicket/Program.cs
@@ -6,28 +6,30 @@ namespace LuckyTicket
     {
         static void Main(string[] args)
         {
+            //Looping to organize re-entry
             while (true)
             {
                 Console.WriteLine("Enter the ticket number (4 to 8 digits)");
                 string numberTicket = Console.ReadLine();
                 int Length = numberTicket.Length-1;
-                long leftpart=0, rigthpart=0;
-                if (Length % 2 != 0) 
+                int leftpart=0, rigthpart=0;
+                //Checking for an even number of digits
+                if (numberTicket.Length % 2 != 0) 
                 {
                     numberTicket = numberTicket.Insert(0, "0");
                     Length += 1;
                 }
-                    
-                
+                //Calculating the sum of the right and left sides of the ticket number
                 for (int i = 0; i<=Length/2;i++) 
                 {
-                    leftpart += long.Parse(numberTicket[i].ToString());
-                    rigthpart += long.Parse(numberTicket[Length - i].ToString());
+                    leftpart += int.Parse(numberTicket[i].ToString());
+                    rigthpart += int.Parse(numberTicket[Length - i].ToString());
                 }
                 if (leftpart == rigthpart)
                     Console.WriteLine("Lucky");
                 else Console.WriteLine("Unlucky");
                 Console.ReadKey();
+                Console.Clear();
             }
         }
     }
